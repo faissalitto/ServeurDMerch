0c5a5b417a6a0c58bd6ea5354f451e70e074a92e
faissalitto <faycalbziou@gmail.com>
2018-10-23 13:57:40 +0100

    first commit

---
+++ src/main/java/com/example/demo/ServeurDMerchApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurDMerchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurDMerchApplication.class, args);
+	}
+}
